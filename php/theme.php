01483b2748b5c16d3ddc4d3eadde4477679315e7
Gogs <gogs@fake.local>
2019-03-10 15:57:43 +0300

    Decription added

--- php/theme.php
+++ php/theme.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- *   * Name: Bluebasic
- *   * Description: This is a variation of the Hubzilla standard theme "Redbasic". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
- *   * Version: Theme Version 20181204<br/><br/>
+ *   * Name: MBNetwork Theme V.2
+ *   * Description: This is the second version of MBNetwork's theme. It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme.
+ *   * Version: Theme Version V.2<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 4.9.9
- *   * Author: boet@nexxt.social
+ *   * Author: Tan Siret AKINCI (Yutyo)
  */
 
 function bluebasic_init(&$a) {
