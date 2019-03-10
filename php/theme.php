178d0c517664b9393605bc93a78acf5235eed04c
Gogs <gogs@fake.local>
2019-03-10 16:42:09 +0300

    update3

--- php/theme.php
+++ php/theme.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- *   * Name: Bluebasic
- *   * Description: This is a variation of the Hubzilla standard theme "Redbasic". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
+ *   * Name: Maker Birliği Network Theme 2
+ *   * Description: This is the second version of MBNetwork theme". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
  *   * Version: Theme Version 20181204<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 4.9.9
