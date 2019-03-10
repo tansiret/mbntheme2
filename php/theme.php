d5014583f7005ef272baf0c2a96f43fbd274caed
Gogs <gogs@fake.local>
2019-03-10 16:43:23 +0300

    addedauthorname

--- php/theme.php
+++ php/theme.php
@@ -2,11 +2,11 @@
 
 /**
  *   * Name: Maker Birliği Network Theme 2
- *   * Description: This is the second version of MBNetwork theme". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
+ *   * Description: This is the second version of MBNetwork theme". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme.
  *   * Version: Theme Version 20181204<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 4.9.9
- *   * Author: boet@nexxt.social
+ *   * Author: Tan Siret AKINCI (Yutyo)
  */
 
 function bluebasic_init(&$a) {
